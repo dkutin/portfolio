26688184147ee5023261bc4d8cb46db556150ae7
Dmitry Kutin <dmitrykutin@outlook.com>
2020-07-01 00:55:25 -0400

    Updates

---
+++ precache-manifest.45d832ba1332f5c58fdc0a696a96f50e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67a9c6c09f3f03e73f37ab845a033a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c9f8935bbc964d0f2c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "4ca83891e705678585f0",
+    "url": "/static/js/2.c953ca69.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.c953ca69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c9f8935bbc964d0f2c",
+    "url": "/static/js/main.e1589bbd.chunk.js"
+  },
+  {
+    "revision": "1713f74845665bcb55a0",
+    "url": "/static/js/runtime-main.c689ea8c.js"
+  }
+]);

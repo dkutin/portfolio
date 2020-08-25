76cc4e51130bacc2c38c8bbc8544c862f517a699
Dmitry Kutin <dmitrykutin@outlook.com>
2020-08-25 17:39:09 -0400

    Update routes, Site data

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ class App extends Component {
 
   render() {
     return (
+      
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route path="/" exact component={App} />
+            <Route
+                path="/fantasytracker" exact
+                component={() => { 
+                    window.location.href = 'https://dmitrykutin.com/fantasytracker'; 
+                    return null;
+                }}
+            />
+            <Route path="*" component={() => {return(
+                <div className="not-found">
+                    <h1>Uh oh! 404 Page Not Found</h1>
+                    <h2>Back to</h2>
+                    <a href="/"> Homepage </a>
+                </div>)}}/>
+        </Switch>
+    </Router>, document.getElementById('root'));
 registerServiceWorker();

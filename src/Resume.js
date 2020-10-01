4bf7a25ff4ce2ccf6e1348ba09cc515ed7387be8
Dmitry Kutin <dmitrykutin@outlook.com>
2020-09-30 22:01:01 -0400

    Add components for 404 and resume pages

---
+++ src/Resume.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+const resumePath = 'pdf/dmitrykutin-resume.pdf'
+
+
+class Resume extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <object data={resumePath} type="application/pdf" width="100%" height="2300px">
+                    <p>Seems the image isn't loading, try following the <a href={resumePath}>link to my resume.</a> </p>
+                </object>
+            </div>
+        )
+    }
+}
+
+export default Resume;
